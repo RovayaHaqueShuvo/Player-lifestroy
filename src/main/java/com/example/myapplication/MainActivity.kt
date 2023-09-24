50e3379ddfe7337cbe14fd7f9c630cfeac2541a2
RovayaHaqueShuvo <rovayahaqueshuvo@gmail.com>
2023-09-24 06:54:45 +0600

    player profile

---
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
---
+++ src/main/java/com/example/myapplication/Player_Mahmudullah.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+class Player_Mahmudullah {
+}
